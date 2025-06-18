4a78d3efda59ace2a943f700e59c14885c74eeb8
Alex <43609447+PegasusXing@users.noreply.github.com>
2025-06-18 11:09:41 +0200

    -- Included GLFW library

--- ProteaCore/vendor/Dependencies.cmake
+++ ProteaCore/vendor/Dependencies.cmake
@@ -1,8 +1,10 @@
-# ////////////////////////////////////////////////////////
-# /// Dependencies: //////////////////////////////////////
-# ////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////
+# /// Dependencies: /////////////////////////////////////////
+# ///////////////////////////////////////////////////////////
 # TODO: Move to manual vendor linking for standalone build
-# ////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////
+
+find_package(OpenGL REQUIRED)
 
 include(FetchContent)
 
@@ -22,4 +24,23 @@ if(NOT spdlog_POPULATED)
     FetchContent_MakeAvailable(spdlog)
     # After fetching, delete the .git folder from the source directory to avoid nested repos
     file(REMOVE_RECURSE "${spdlog_SOURCE_DIR}/.git")
+endif()
+
+
+FetchContent_Declare(
+        glfw
+        GIT_REPOSITORY              https://github.com/glfw/glfw.git
+        GIT_TAG                     3.4
+        GIT_SHALLOW                 TRUE
+)
+
+# Prevent multiple fetches in multi-config generators (e.g., Visual Studio)
+set(glfw_POPULATE 1 CACHE INTERNAL "Mark glfw as already populated")
+
+FetchContent_GetProperties(glfw)
+
+if(NOT glfw_POPULATED)
+    FetchContent_MakeAvailable(glfw)
+    # After fetching, delete the .git folder from the source directory to avoid nested repos
+    file(REMOVE_RECURSE "${glfw_SOURCE_DIR}/.git")
 endif()
