8970f4f40001f983807a84cb2915f476f3b2ff97
Alex <43609447+PegasusXing@users.noreply.github.com>
2025-06-18 19:39:23 +0200

    -- Added GLAD

--- ProteaCore/source/Platform/Windows/WindowsWindow.cpp
+++ ProteaCore/source/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,12 @@
 #include "ProteaPCH.h"
 #include "WindowsWindow.h"
 
+#include <glad/glad.h>
+
 #include "Protea/Events/ApplicationEvent.h"
 #include "Protea/Events/MouseEvent.h"
 #include "Protea/Events/KeyEvent.h"
+
 
 namespace Protea {
     static bool s_GLFWInitialized = false;
@@ -43,6 +46,8 @@ namespace Protea {
 
         m_Window = glfwCreateWindow((int) props.Width, (int) props.Height, m_Data.Title.c_str(), nullptr, nullptr);
         glfwMakeContextCurrent(m_Window);
+        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+        PROTEA_CORE_ASSERT(status, "Failed to initialize Glad!");
         glfwSetWindowUserPointer(m_Window, &m_Data);
         SetVSync(true);
 
--- ProteaCore/vendor/Dependencies.cmake
+++ ProteaCore/vendor/Dependencies.cmake
@@ -1,13 +1,12 @@
-# ///////////////////////////////////////////////////////////
-# /// Dependencies: /////////////////////////////////////////
-# ///////////////////////////////////////////////////////////
-# TODO: Move to manual vendor linking for standalone build
-# ///////////////////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////
+# /// Dependencies: ////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////
 
 find_package(OpenGL REQUIRED)
 
 include(FetchContent)
 
+# SPDLOG
 FetchContent_Declare(
         spdlog
         GIT_REPOSITORY              https://github.com/gabime/spdlog.git
@@ -15,18 +14,19 @@ FetchContent_Declare(
         GIT_SHALLOW                 TRUE
 )
 
-# Prevent multiple fetches in multi-config generators (e.g., Visual Studio)
 set(spdlog_POPULATE 1 CACHE INTERNAL "Mark spdlog as already populated")
 
 FetchContent_GetProperties(spdlog)
 
 if(NOT spdlog_POPULATED)
     FetchContent_MakeAvailable(spdlog)
-    # After fetching, delete the .git folder from the source directory to avoid nested repos
     file(REMOVE_RECURSE "${spdlog_SOURCE_DIR}/.git")
 endif()
 
-
+# GLFW
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
         glfw
         GIT_REPOSITORY              https://github.com/glfw/glfw.git
@@ -34,13 +34,28 @@ FetchContent_Declare(
         GIT_SHALLOW                 TRUE
 )
 
-# Prevent multiple fetches in multi-config generators (e.g., Visual Studio)
 set(glfw_POPULATE 1 CACHE INTERNAL "Mark glfw as already populated")
 
 FetchContent_GetProperties(glfw)
 
 if(NOT glfw_POPULATED)
     FetchContent_MakeAvailable(glfw)
-    # After fetching, delete the .git folder from the source directory to avoid nested repos
     file(REMOVE_RECURSE "${glfw_SOURCE_DIR}/.git")
+endif()
+
+# GLAD
+FetchContent_Declare(
+        glad
+        GIT_REPOSITORY              https://github.com/PegasusXing/glad.git
+        GIT_TAG                     0.1.36 # main branch
+        GIT_SHALLOW                 TRUE
+)
+
+set(glad_POPULATE 1 CACHE INTERNAL "Mark glad as already populated")
+
+FetchContent_GetProperties(glad)
+
+if(NOT glad_POPULATED)
+    FetchContent_MakeAvailable(glad)
+    file(REMOVE_RECURSE "${glad_SOURCE_DIR}/.git")
 endif()
