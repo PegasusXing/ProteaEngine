5ce55c43226a27e3240112ad181969cfa2f19f4f
Alex <43609447+PegasusXing@users.noreply.github.com>
2025-06-19 13:35:30 +0200

    -- Updated build.bat to include configurations for different builds.

--- build.bat
+++ build.bat
@@ -18,14 +18,16 @@ if %ver% GEQ 10 (
     )
 )
 
-:: Menu :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :MAIN_MENU
 cls
 echo -----------------------------------------------------------------------------
+echo -----------------------------------------------------------------------------
 echo ------------------------  %COLOR_GREEN%ProteaEngine Build System%COLOR_RESET%  ------------------------
 echo -----------------------------------------------------------------------------
-echo ------- %COLOR_YELLOW%NOTE: GCC MAY FAIL IF ABOVE C++17 IS REQUIRED%COLOR_RESET% -------
+echo ------- %COLOR_YELLOW%NOTE: GCC DOESN'T WORK DUE TO CXX STANDARD BEING ABOVE CXX 17%COLOR_RESET% -------
 echo -----------------------------------------------------------------------------
+echo -----------------------------------------------------------------------------
+echo.
 echo 1. %COLOR_GREEN%Clang%COLOR_RESET%   (with Ninja)
 echo 2. %COLOR_GREEN%GCC%COLOR_RESET%     (with Ninja)
 echo 3. %COLOR_GREEN%MSVC%COLOR_RESET%    (Visual Studio 2022)
@@ -44,72 +46,49 @@ echo Invalid choice, please try again.
 pause
 goto MAIN_MENU
 
-:: Build Definitions ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
 :CLANG_BUILD
-set COMPILER_NAME=clang
-set C_COMPILER=clang
-set CXX_COMPILER=clang++
-set GENERATOR=Ninja
-goto SINGLE_CONFIG_BUILD
+cls
+echo [Clang] Configuring and Building with Clang and Ninja...
+set BUILD_DIR=build-clang
+set GENERATOR="Ninja"
+set COMPILER_FLAGS=-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+goto CONFIGURE_BUILD
 
 :GCC_BUILD
-set COMPILER_NAME=gcc
-set C_COMPILER=gcc
-set CXX_COMPILER=g++
-set GENERATOR=Ninja
-goto SINGLE_CONFIG_BUILD
-
-:SINGLE_CONFIG_BUILD
-for %%C in (Debug Release Dist) do (
-    set BUILD_DIR=build-%COMPILER_NAME%-%%C
-    echo [%COMPILER_NAME% / %%C] Configuring in !BUILD_DIR!...
-    if not exist !BUILD_DIR! mkdir !BUILD_DIR!
-    pushd !BUILD_DIR!
-    cmake -G %GENERATOR% -DCMAKE_C_COMPILER=%C_COMPILER% -DCMAKE_CXX_COMPILER=%CXX_COMPILER% -DCMAKE_BUILD_TYPE=%%C ..
-    if !errorlevel! neq 0 (
-        echo %COLOR_RED%CMake configuration failed for %%C!%COLOR_RESET%
-        popd
-        pause
-        goto MAIN_MENU
-    )
-    cmake --build .
-    if !errorlevel! neq 0 (
-        echo %COLOR_RED%Build failed for %%C!%COLOR_RESET%
-        popd
-        pause
-        goto MAIN_MENU
-    )
-    popd
-)
-echo %COLOR_GREEN%All builds completed for %COMPILER_NAME%.%COLOR_RESET%
-pause
-goto MAIN_MENU
+cls
+echo [GCC] Configuring and Building with GCC and Ninja...
+set BUILD_DIR=build-gcc
+set GENERATOR="Ninja"
+set COMPILER_FLAGS=-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+goto CONFIGURE_BUILD
 
 :MSVC_BUILD
+cls
+echo [MSVC] Configuring and Building with Visual Studio 2022...
 set BUILD_DIR=build-msvc
-echo [MSVC] Configuring in %BUILD_DIR%...
+set GENERATOR="Visual Studio 17 2022"
+set COMPILER_FLAGS=-A x64 -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
+goto CONFIGURE_BUILD
+
+:CONFIGURE_BUILD
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
-pushd %BUILD_DIR%
-cmake -G "Visual Studio 17 2022" -A x64 ..
-if !errorlevel! neq 0 (
-    echo %COLOR_RED%CMake configuration failed for MSVC!%COLOR_RESET%
-    popd
+cd %BUILD_DIR%
+
+cmake -G %GENERATOR% %COMPILER_FLAGS% -DCMAKE_BUILD_TYPE=Debug ..
+if %errorlevel% neq 0 (
+    echo CMake configuration failed!
     pause
-    goto MAIN_MENU
+    exit /b
 )
 
-for %%C in (Debug Release Dist) do (
-    echo [MSVC / %%C] Building...
-    cmake --build . --config %%C
-    if !errorlevel! neq 0 (
-        echo %COLOR_RED%Build failed for %%C!%COLOR_RESET%
-        popd
-        pause
-        goto MAIN_MENU
-    )
+cmake --build .
+if %errorlevel% neq 0 (
+    echo Build failed!
+    pause
+    exit /b
 )
-popd
-echo %COLOR_GREEN%All builds completed for MSVC.%COLOR_RESET%
+
+echo Build completed successfully in %BUILD_DIR% directory.
+cd ..
 pause
 goto MAIN_MENU
