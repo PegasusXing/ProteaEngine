be51713617fbc127b78368c8c05e4f2680f19abb
Alex <43609447+PegasusXing@users.noreply.github.com>
2025-06-21 23:56:46 +0200

    -- Shader load from file

--- ProteaCore/source/Platform/OpenGL/OpenGLShader.h
+++ ProteaCore/source/Platform/OpenGL/OpenGLShader.h
@@ -9,11 +9,15 @@
 #include "Protea/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO: REMOVE!!!
+typedef unsigned int GLenum;
+
 namespace Protea {
 
     class OpenGLShader : public Shader
     {
     public:
+        OpenGLShader(const std::string& filepath);
         OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
         virtual ~OpenGLShader();
 
@@ -30,6 +34,10 @@ namespace Protea {
         void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
         void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
     private:
+        std::string ReadFile(const std::string& filepath);
+        std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+        void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
+
         uint32_t m_RendererID;
     };
 
--- ProteaCore/source/Protea/Renderer/Shader.cpp
+++ ProteaCore/source/Protea/Renderer/Shader.cpp
@@ -5,15 +5,25 @@
 #include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Protea {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    PROTEA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		PROTEA_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
-
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:    PROTEA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 			case RendererAPI::API::OpenGL:  return new OpenGLShader(vertexSrc, fragmentSrc);
 		}
-
 		PROTEA_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
     }
