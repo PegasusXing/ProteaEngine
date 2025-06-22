cb0adf3a354e769f28eec7f5bde872a31c3f4717
Alex <43609447+PegasusXing@users.noreply.github.com>
2025-06-22 18:26:03 +0200

    -- Added Ortho camera controller class

---
+++ ProteaCore/source/Protea/OrthographicCameraController.h
@@ -0,0 +1,40 @@
+#ifndef ORTHOGRAPHICCAMERACONTROLLER_H
+#define ORTHOGRAPHICCAMERACONTROLLER_H
+
+#include "Protea/Renderer/OrthographicCamera.h"
+#include "Protea/Core/Timestep.h"
+
+#include "Protea/Events/ApplicationEvent.h"
+#include "Protea/Events/MouseEvent.h"
+
+namespace Protea {
+
+    class OrthographicCameraController
+    {
+    public:
+        OrthographicCameraController(float aspectRatio, bool rotation = false);
+
+        void OnUpdate(Timestep ts);
+        void OnEvent(Event& e);
+
+        OrthographicCamera& GetCamera() { return m_Camera; }
+        const OrthographicCamera& GetCamera() const { return m_Camera; }
+    private:
+        bool OnMouseScrolled(MouseScrolledEvent& e);
+        bool OnWindowResized(WindowResizeEvent& e);
+
+        float m_AspectRatio;
+        float m_ZoomLevel = 1.0f;
+        OrthographicCamera m_Camera;
+
+        bool m_Rotation;
+
+        glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
+        float m_CameraRotation = 0.0f;
+        float m_CameraTranslationSpeed = 5.0f, m_CameraRotationSpeed = 180.0f;
+    };
+
+}
+
+
+#endif //ORTHOGRAPHICCAMERACONTROLLER_H
--- ProteaCore/source/Protea/Renderer/OrthographicCamera.h
+++ ProteaCore/source/Protea/Renderer/OrthographicCamera.h
@@ -13,6 +13,7 @@ namespace Protea {
     {
     public:
         OrthographicCamera(float left, float right, float bottom, float top);
+        void SetProjection(float left, float right, float bottom, float top);
 
         const glm::vec3& GetPosition() const { return m_Position; }
         void SetPosition(const glm::vec3& position) { m_Position = position; RecalculateViewMatrix(); }
